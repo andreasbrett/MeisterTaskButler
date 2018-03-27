54b45314b0c32976f0eccbdc4610cbd6cf9b6eaf
Andreas Brett <andreasbrett@users.noreply.github.com>
2018-03-27 14:37:21 +0200

    Add files via upload

---
+++ smtp.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.Utils import COMMASPACE, formatdate
+from email import Encoders
+import os
+
+def sendMail(toAddresses, fromAddress, mailSubject, mailBody, server = "localhost"):
+	# define mail's metadata
+	oMessage = MIMEMultipart()
+	oMessage['To'] = COMMASPACE.join(toAddresses)
+	oMessage['From'] = fromAddress
+	oMessage['Date'] = formatdate(localtime=True)
+	oMessage['Subject'] = mailSubject
+	
+	# set mail body
+	oMessage.attach(MIMEText(mailBody))
+
+	# send mail
+	smtp = smtplib.SMTP(server)
+	smtp.sendmail(fromAddress, toAddresses, oMessage.as_string() )
+	smtp.close()
