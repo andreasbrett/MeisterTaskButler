6c40ed55862cbc8d03955a6b005b3eb2a2091c1f
Andreas Brett <andreasbrett@users.noreply.github.com>
2020-05-01 12:30:43 +0200

    migrate from python27 to python3

--- smtp.py
+++ smtp.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEBase import MIMEBase
-from email.MIMEText import MIMEText
-from email.Utils import COMMASPACE, formatdate
-from email import Encoders
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+from email import encoders
 import os
 
 def sendMail(toAddresses, fromAddress, mailSubject, mailBodyHtml, server = "localhost"):
